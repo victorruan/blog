5d5a57a7225c75ed3c9d77d4e2a851dcd0b24ac4
阮佳佳 <250069802@qq.com>
2016-08-26 18:07:56 +0800

    自动构建

--- src/app/views/layout.php
+++ src/app/views/layout.php
@@ -40,7 +40,25 @@
     })();
 </script>
 <!--统计代码 end-->
-
+<script src='http://cdn.bootcss.com/socket.io/1.3.7/socket.io.js'></script>
+<script>
+    // 连接服务端
+    var socket = io('http://workerman.net:2120');
+    // uid可以是自己网站的用户id，以便针对uid推送以及统计在线人数
+    uid = 123;
+    // socket连接后以uid登录
+    socket.on('connect', function(){
+        socket.emit('login', uid);
+    });
+    // 后端推送来消息时
+    socket.on('new_msg', function(msg){
+        console.log("收到消息："+msg);
+    });
+    // 后端推送来在线数据时
+    socket.on('update_online_count', function(online_stat){
+        console.log(online_stat);
+    });
+</script>
 </body>
 <style>
     .ds-thread{border: 1px solid #DDDDDD;border-radius: 3px;padding: 10px;}
--- src/app/views/post.php
+++ src/app/views/post.php
@@ -1,3 +1,21 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="/"><img src="https://avatars1.githubusercontent.com/u/6701576?v=3&s=460" alt="victorruan logo" width="50px"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="/">首页</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <div style="padding: 20px;border: 1px solid #DDDDDD;border-radius: 3px;margin-bottom: 20px ">
     <fieldset>
         <legend align="center"><?=$_title?></legend>
