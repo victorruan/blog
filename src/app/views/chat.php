bfe2840398f36de294f9c0d62f3bb1caeff85a27
阮佳佳 <250069802@qq.com>
2016-09-02 17:41:26 +0800

    自动构建

--- src/app/views/chat.php
+++ src/app/views/chat.php
@@ -8,7 +8,7 @@
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body { font: 13px Helvetica, Arial; }
         form { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
-        form input { border: 0; padding: 10px;  margin-right: .5%; }
+        form input { border: 0; padding: 10px;   }
         form button {  background: rgb(130, 224, 255); border: none; padding: 10px; }
         #messages { list-style-type: none; margin: 0; padding: 0; }
         #messages li { padding: 5px 10px; }
