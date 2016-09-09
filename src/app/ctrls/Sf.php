46c508257d2f87c95b4eb18d0057b05c5f70af05
阮佳佳 <250069802@qq.com>
2016-09-09 17:46:17 +0800

    自动构建

---
+++ src/app/ctrls/Sf.php
@@ -0,0 +1,11 @@
+<?php
+namespace VictorRuan\app\ctrls;
+use VictorRuan\base\Ctrl;
+class Sf extends Ctrl
+{
+   public function index(){
+       array_walk(get_sf_questions(),function($v,$k){
+           echo "<a href=\"{$k}\">{$v}</a>";
+       });
+   }
+}
