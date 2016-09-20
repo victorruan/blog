82a3fea1f5d7bc8c81b0c6330b1824c3ca659ef8
阮佳佳 <250069802@qq.com>
2016-09-20 14:53:44 +0800

    自动构建

--- src/app/views/post.php
+++ src/app/views/post.php
@@ -4,7 +4,7 @@
         <?=$html?>
         <?php if(is_login()) :?>
             <div class="row" style="float: right">
-                <a href="/p/edit?id=<?=$id?>">编辑此文</a>
+                <a href="/p/edit/<?=$id?>">编辑此文</a>
                 <a href="/p/edit">添加新文</a>
             </div>
         <?php endif?>
